a95a8dc635ed2208a1aeaa67e5fcf37a4c3e1b09
Umar <mals.umar@mail.ru>
2021-03-14 16:01:59 +0300

    added database and loading quotes from that said database to ListWidget

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
 from PyQt5 import uic
 import pyperclip
 import sqlite3
@@ -11,21 +12,27 @@ class QuoteBrowser(QMainWindow):
     def __init__(self):
         super(QuoteBrowser, self).__init__()
         uic.loadUi('data/design.ui', self)
+        self.con = QSqlDatabase.addDatabase("QSQLITE")
+        self.con.setDatabaseName('data/quotes.sqlite')
         self.show_quotes()
 
     def show_quotes(self):
-        db = self.get_db_cursor()
         quotes = self.get_quotes_from_db()
-        self.load_quotes_to_ListWidget()
+        self.load_quotes_to_application(quotes)
 
-    def load_quotes_to_ListWidget(self):
-        pass
-
-    def get_db(self):
-        pass
+    def load_quotes_to_application(self, quotes):
+        for quote in quotes:
+            self.QuoteList.addItem(quote)
 
     def get_quotes_from_db(self):
-        pass
+        quotes = []
+        self.con.open()
+        query = QSqlQuery()
+        query.exec('SELECT quote FROM quotes')
+        while query.next():
+            quotes.append(query.value(0))
+        self.con.close()
+        return quotes
 
 
 if __name__ == '__main__':
