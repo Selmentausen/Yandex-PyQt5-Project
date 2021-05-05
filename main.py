4e8711770cee6c6fe95cdfc789ec9698dd8828a0
Selmentausen <Selmentausen@gmail.com>
2021-05-05 18:49:40 +0300

    User can delete quotes added by himself

--- main.py
+++ main.py
@@ -1,146 +1,12 @@
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QWidget,
                              QHBoxLayout, QLabel, QFrame, QGridLayout,
-                             QPushButton, QComboBox, QLayout, QTextBrowser)
+                             QPushButton, QComboBox)
 from PyQt5.QtSql import QSqlDatabase, QSqlQuery
 from PyQt5 import uic
 import sys
 
 
-class QuoteBrowser(QMainWindow):
-    libraryQuotesLayout: QLayout
-    bookmarksQuotesLayout: QLayout
-
-    def __init__(self):
-        super(QuoteBrowser, self).__init__()
-        uic.loadUi('data/design.ui', self)
-        self.con = QSqlDatabase.addDatabase("QSQLITE")
-        self.con.setDatabaseName('data/db/quotes_db.sqlite')
-        self.initUi()
-
-    def initUi(self):
-        self.bookmarksButton.clicked.connect(self.change_page)
-        self.libraryButton.clicked.connect(self.change_page)
-        self.myQuotesButton.clicked.connect(self.change_page)
-        self.addQuoteButton.clicked.connect(self.add_quote)
-        self.stackedWidget.currentChanged.connect(self.load_quotes)
-        self.load_quotes()
-
-    def change_page(self):
-        text = self.sender().text()
-        if text == 'Library':
-            self.stackedWidget.setCurrentIndex(0)
-        elif text == 'Bookmarks':
-            self.stackedWidget.setCurrentIndex(1)
-        elif text == 'My Quotes':
-            self.stackedWidget.setCurrentIndex(2)
-
-    def create_quote(self, quote, author) -> QFrame:
-        main_frame = QFrame()
-        main_frame_layout = QGridLayout()
-        main_frame.setLayout(main_frame_layout)
-
-        author_frame = QFrame()
-        author_frame.setFrameShape(QFrame.Panel)
-        author_layout = QHBoxLayout()
-        author_layout.addWidget(QLabel(author['name']))
-        author_layout.addWidget(QLabel(author['birthday']))
-        author_frame.setLayout(author_layout)
-
-        quote_frame = QFrame()
-        quote_frame.setFrameShape(QFrame.Panel)
-        quote_layout = QVBoxLayout()
-        text = QLabel(quote['text'])
-        quote_layout.addWidget(text)
-        quote_frame.setLayout(quote_layout)
-
-        # TODO if user is author add two button delete and edit
-        button_frame = QFrame()
-        button_layout = QHBoxLayout()
-        quote_button_bookmark = QPushButton(
-            'Add to bookmarks' if quote['bookmarked'] == 'False' else 'Remove from bookmarks')
-        quote_button_bookmark.clicked.connect(self.add_quote_to_bookmarks)
-        button_layout.addWidget(quote_button_bookmark)
-        button_frame.setLayout(button_layout)
-
-        main_frame.setFrameShape(QFrame.Box)
-        main_frame_layout.addWidget(author_frame, 0, 0)
-        main_frame_layout.addWidget(quote_frame, 1, 0, 3, 1)
-        main_frame_layout.addWidget(button_frame, 4, 0)
-        return main_frame
-
-    def add_quote_to_bookmarks(self):
-        # Get quote block GridLayout where quote Label is stored
-        layout: QGridLayout = self.sender().parentWidget().parentWidget().findChild(QGridLayout)
-        # Get quote Label from GridLayout
-        label = layout.itemAtPosition(1, 0).widget().findChild(QLabel)
-        flag = True if self.sender().text() == 'Add to bookmarks' else False
-        self.con.open()
-        print('hello', flag)
-        QSqlQuery(f"""
-        UPDATE quotes
-        SET bookmarked = "{flag}"
-        WHERE text == "{label.text()}"
-        """)
-        self.con.close()
-        self.load_quotes()
-
-    def load_quotes(self):
-        quotes = self.get_quotes_from_db()
-        authors = self.get_authors_from_db()
-
-        for i in reversed(range(self.libraryQuotesLayout.count())):
-            self.libraryQuotesLayout.itemAt(i).widget().setParent(None)
-        for i in reversed(range(self.bookmarksQuotesLayout.count())):
-            self.bookmarksQuotesLayout.itemAt(i).widget().setParent(None)
-
-        for quote in quotes:
-            quote_author = next((a for a in authors if a['id'] == quote['author_id']), None)
-            self.libraryQuotesLayout.addWidget(self.create_quote(quote, quote_author))
-            if quote['bookmarked'] == 'True':
-                self.bookmarksQuotesLayout.addWidget(self.create_quote(quote, quote_author))
-
-    def get_authors_from_db(self):
-        self.con.open()
-        query = QSqlQuery()
-        author_columns = ['id', 'name', 'image_path', 'birthday']
-        query.exec(f'SELECT {", ".join(author_columns)} FROM authors')
-        authors = self.get_query_dict(query, author_columns)
-        self.con.close()
-        return authors
-
-    def get_quotes_from_db(self):
-        self.con.open()
-        quote_columns = ['id', 'text', 'bookmarked', 'author_id']
-        query = QSqlQuery()
-        query.exec(f'SELECT {", ".join(quote_columns)} FROM quotes')
-        quotes = self.get_query_dict(query, quote_columns)
-        self.con.close()
-        return quotes
-
-    def get_bookmarks_from_db(self):
-        self.con.open()
-        bookmark_columns = ['id', 'text', 'author_id']
-        query = QSqlQuery()
-        query.exec(f'SELECT {", ".join(bookmark_columns)} FROM quotes WHERE bookmarked == "True"')
-        bookmarks = self.get_query_dict(query, bookmark_columns)
-        self.con.close()
-        return bookmarks
-
-    @staticmethod
-    def get_query_dict(query, columns):
-        elements = []
-        while query.next():
-            elements.append({key: query.value(key) for key in columns})
-        return elements
-
-    def add_quote(self):
-        self.quote_adder = AddQuoteWindow(self.con)
-        self.quote_adder.show()
-
-
 class AddQuoteWindow(QWidget):
-    chooseAuthorComboBox: QComboBox
-
     def __init__(self, con):
         super(AddQuoteWindow, self).__init__()
         uic.loadUi('data/add_quote.ui', self)
@@ -196,11 +62,169 @@ class AddQuoteWindow(QWidget):
         data = self.get_data()
         self.con.open()
         QSqlQuery(f"""
-        INSERT INTO quotes (text, author_id)
-        VALUES ("{data['quote']}", (SELECT id FROM authors WHERE name == "{data['name']}"))
+        INSERT INTO quotes (text, author_id, bookmarked, user_added)
+        VALUES ("{data['quote']}", (SELECT id FROM authors WHERE name == "{data['name']}"), "False", "True")
         """)
         self.con.close()
         self.close()
+
+
+class QuoteBrowser(QMainWindow):
+    quote_adder: AddQuoteWindow
+
+    def __init__(self):
+        super(QuoteBrowser, self).__init__()
+        uic.loadUi('data/design.ui', self)
+        self.con = QSqlDatabase.addDatabase("QSQLITE")
+        self.con.setDatabaseName('data/db/quotes_db.sqlite')
+        self.initUi()
+
+    def initUi(self):
+        self.bookmarksButton.clicked.connect(self.change_page)
+        self.libraryButton.clicked.connect(self.change_page)
+        self.myQuotesButton.clicked.connect(self.change_page)
+        self.addQuoteButton.clicked.connect(self.add_quote)
+        self.exitButton.clicked.connect(self.close)
+        self.stackedWidget.currentChanged.connect(self.update_quotes)
+        self.update_quotes()
+
+    def change_page(self):
+        text = self.sender().text()
+        if text == 'Library':
+            self.stackedWidget.setCurrentIndex(0)
+        elif text == 'Bookmarks':
+            self.stackedWidget.setCurrentIndex(1)
+        elif text == 'My Quotes':
+            self.stackedWidget.setCurrentIndex(2)
+
+    def create_quote(self, quote, author) -> QFrame:
+        main_frame = QFrame()
+        main_frame_layout = QGridLayout()
+        main_frame.setLayout(main_frame_layout)
+
+        author_frame = QFrame()
+        author_frame.setFrameShape(QFrame.Panel)
+        author_layout = QHBoxLayout()
+        author_layout.addWidget(QLabel(f"Author: {author['name']}"))
+        author_layout.addWidget(QLabel(f"Birthday: {author['birthday']}"))
+        author_frame.setLayout(author_layout)
+
+        quote_frame = QFrame()
+        quote_frame.setFrameShape(QFrame.Panel)
+        quote_layout = QVBoxLayout()
+        text = QLabel(quote['text'])
+        quote_layout.addWidget(text)
+        quote_frame.setLayout(quote_layout)
+
+        button_frame = QFrame()
+        button_layout = QHBoxLayout()
+        if quote['user_added'] == 'True':
+            quote_button_delete = QPushButton('Delete quote')
+            quote_button_delete.clicked.connect(self.delete_quote)
+            quote_button_edit = QPushButton('Edit quote')
+            quote_button_edit.clicked.connect(self.edit_quote)
+            button_layout.addWidget(quote_button_delete)
+            button_layout.addWidget(quote_button_edit)
+        if author['name'] != 'You':
+            quote_button_bookmark = QPushButton(
+                'Add to bookmarks' if quote['bookmarked'] == 'False' else 'Remove from bookmarks')
+            quote_button_bookmark.clicked.connect(self.add_quote_to_bookmarks)
+            button_layout.addWidget(quote_button_bookmark)
+        button_frame.setLayout(button_layout)
+
+        main_frame.setFrameShape(QFrame.Box)
+        main_frame_layout.addWidget(author_frame, 0, 0)
+        main_frame_layout.addWidget(quote_frame, 1, 0, 3, 1)
+        main_frame_layout.addWidget(button_frame, 4, 0)
+        return main_frame
+
+    def add_quote_to_bookmarks(self):
+        label = self.get_label_from_quote_block(self.sender())
+        flag = True if self.sender().text() == 'Add to bookmarks' else False
+        self.con.open()
+        QSqlQuery(f"""
+        UPDATE quotes
+        SET bookmarked = "{flag}"
+        WHERE text == "{label.text()}"
+        """)
+        self.con.close()
+        self.update_quotes()
+
+    def delete_quote(self):
+        label = self.get_label_from_quote_block(self.sender())
+        self.con.open()
+        QSqlQuery(f"""
+        DELETE FROM quotes
+        WHERE text == "{label.text()}"
+        """)
+        self.update_quotes()
+
+    def edit_quote(self):
+        pass    # TODO implement ability to edit quotes
+
+    def update_quotes(self):
+        quotes = self.get_quotes_from_db()
+        authors = self.get_authors_from_db()
+
+        self.clear_quote_layouts()
+        for quote in quotes:
+            quote_author = next((a for a in authors if a['id'] == quote['author_id']), None)
+            self.libraryQuotesLayout.addWidget(self.create_quote(quote, quote_author))
+            if quote['bookmarked'] == 'True':
+                self.bookmarksQuotesLayout.addWidget(self.create_quote(quote, quote_author))
+            if quote_author['name'] == 'You':
+                self.myQuotesLayout.addWidget(self.create_quote(quote, quote_author))
+
+    def clear_quote_layouts(self):
+        for layout in [self.libraryQuotesLayout, self.bookmarksQuotesLayout, self.myQuotesLayout]:
+            for i in reversed(range(layout.count())):
+                layout.itemAt(i).widget().setParent(None)
+
+    def get_authors_from_db(self):
+        self.con.open()
+        query = QSqlQuery()
+        author_columns = ['id', 'name', 'image_path', 'birthday']
+        query.exec(f'SELECT {", ".join(author_columns)} FROM authors')
+        authors = self.get_query_dict(query, author_columns)
+        self.con.close()
+        return authors
+
+    def get_quotes_from_db(self):
+        self.con.open()
+        quote_columns = ['id', 'text', 'bookmarked', 'author_id', 'user_added']
+        query = QSqlQuery()
+        query.exec(f'SELECT {", ".join(quote_columns)} FROM quotes')
+        quotes = self.get_query_dict(query, quote_columns)
+        self.con.close()
+        return quotes
+
+    def get_bookmarks_from_db(self):
+        self.con.open()
+        bookmark_columns = ['id', 'text', 'author_id']
+        query = QSqlQuery()
+        query.exec(f'SELECT {", ".join(bookmark_columns)} FROM quotes WHERE bookmarked == "True"')
+        bookmarks = self.get_query_dict(query, bookmark_columns)
+        self.con.close()
+        return bookmarks
+
+    @staticmethod
+    def get_query_dict(query, columns):
+        elements = []
+        while query.next():
+            elements.append({key: query.value(key) for key in columns})
+        return elements
+
+    @staticmethod
+    def get_label_from_quote_block(button):
+        # Get quote block GridLayout where quote Label is stored
+        layout = button.sender().parentWidget().parentWidget().findChild(QGridLayout)
+        # Get quote Label by position from GridLayout
+        label = layout.itemAtPosition(1, 0).widget().findChild(QLabel)
+        return label
+
+    def add_quote(self):
+        self.quote_adder = AddQuoteWindow(self.con)
+        self.quote_adder.show()
 
 
 def except_hook(cls, exception, traceback):
